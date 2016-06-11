f3ec4ae19a32874470be51527bd1ac92992ed140
dima <ischenko.dmitry@gmail.com>
2016-06-11 21:35:47 +0300

    new data for 24 exp

--- src/main-check.R
+++ src/main-check.R
@@ -10,3 +10,10 @@ for (e.name in c("1a", "1b", "1c", "2a", "2b", "2c")) {
   # generate report
   generate_report(odirt = cur.exp, wdir = sprintf("rep/%s", e.name))
 }
+
+for (e.name in as.character(2:24)) {
+  # load exp
+  cur.exp <- idirt(file = "data/idirt/s_24_new.csv", exp.name = e.name)
+  # generate report
+  generate_report(odirt = cur.exp, wdir = sprintf("rep/%s", e.name))
+}
