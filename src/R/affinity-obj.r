a723db7ad53d28ebd1948ff3657aceeb79548fa7
dima <ischenko.dmitry@gmail.com>
2016-06-14 18:33:55 +0300

    affinity significance test and histogram

--- src/R/affinity-obj.r
+++ src/R/affinity-obj.r
@@ -19,11 +19,13 @@ affinity <- function(exp, ...) UseMethod("affinity")
 #'   H/L, L/H, H/(H+L) or L/(H+L) (!not used now)
 #' @param use.normalized Use or not Normalized ratios of H/L to
 #'   calculate affinity
-#' @param normalization Type of normalization (now not used)
+#' @param protnames Specified names of selected proteins from
+#'   mqexp object, if protnames is empty character ("") subset
+#'   first protein for each protein group
 #' @export
-affinity.mqexp <- function(exp, minpep = 3, avalue = "H/(H+L)",
-                           use.normalized = T,
-                           normalization = "") {
+affinity.mqexp <- function(exp, minpep, avalue,
+                           use.normalized,
+                           protnames = c(""), ...) {
   # check avalue
   posv <- c("H/L", "L/H", "H/(H+L)", "L/(H+L)")
   if (! avalue %in% posv) {
@@ -40,7 +42,16 @@ affinity.mqexp <- function(exp, minpep = 3, avalue = "H/(H+L)",
                    dt$hln, dt$hl)
   # calculate affinity value
   dt$aff <- (dt$val^-1 + 1)^-1
-  return(dt[, c("protein", "pg", "aff"), with = F])
+  # subset proteins
+  if (protnames == "") {
+    dt <- dt[, .SD[1], by = list(pg)]
+  } else {
+    dt <- dt[dt$protein %in% protnames, ]
+  }
+  aff <- dt[, c("protein", "aff"), with = F]
+  class(aff) <- c("affinity", class(aff))
+  
+  return(aff)
 }
 
 #' @export
@@ -49,3 +60,64 @@ affinity.default <- function(exp, ...) {
        class(exp)))
 }
 
+#' Check for significance for each protein in affinity object
+#'
+#' @param dt Affinity object
+#' 
+#' @export
+signif_test <- function(dt, ...) UseMethod("signif_test")
+
+#' 
+#' @param apply.chauven Logical, apply or not Chauvenet filtration for
+#'   affinity value vector
+#' @param maxit Maximal number of iteration in Chauvenet filtration procedure
+#' @param ch.th Chauvenet filtration coefficient
+#' @param add.plot Plot histogram with significance
+#' @param plot.sign Significance to mark values at histogram
+#' 
+#' @return vector with significance for each protein
+#' @export
+signif_test.affinity <- function(dt, apply.chauven, max.it = 10, ch.th = 2,
+                                 add.plot, plot.sign = 1e-2, ...) {
+  tval <- dt$aff
+  
+  # Chauvenet filtration
+  if (apply.chauven) {
+    n.it <- 0
+    while(n.it < max.it) {
+      tval.n <- ChauvenetFilter(tval, th = ch.th)
+      if (length(tval.n) == length(tval)) break
+      tval <- tval.n
+      n.it <- n.it + 1
+    }
+  }
+  dt.mean <- mean(tval)
+  dt.sd <- sd(tval)
+  pv <- pnorm(q = dt$aff, mean = dt.mean, sd = dt.sd, lower.tail = F)
+  names(pv) <- dt$protein
+  
+  # add histogram
+  pv.th <- plot.sign
+  hl.th <- qnorm(1 - pv.th, mean = dt.mean, sd = dt.sd)
+  if (add.plot) {
+    v.brks <- seq(min(dt$aff), max(dt$aff), length.out = 50)
+    v.clr <- rep("white", 50)
+    v.fdb <- min(which(v.brks >= hl.th))
+    if (v.fdb == Inf) { v.fdb <- length(v.brks)}
+    v.clr[(v.fdb - 1):length(v.clr)] <- "dodgerblue"
+    hist(dt$aff, prob = T,
+         main = "Affinity histogram", xlab = "Affinity",
+         breaks = v.brks, col = v.clr)
+    lines(density(rnorm(n = 10*nrow(dt), mean = dt.mean, sd = dt.sd)),
+          col = "red", lwd = 2)
+    abline(v = hl.th, lty = 2, col = "gray30")
+  }
+  
+  return(pv)
+}
+
+#' @export
+signif_test.default <- function(dt, ...) {
+  stop(sprintf("This class (%s) of experiment doesn't supported.",
+               class(dt)))
+}
--- src/main-check.r
+++ src/main-check.r
@@ -11,6 +11,18 @@ cur.exp <- mqexp(file = "data/idirt/s_24_new.csv", "12")
 # generate report
 generate_report(cur.exp, wdir = "rep/1a")
 
-# get affinity object
+# get affinity object (now only first protein for each protein group)
+#  remove contaminants and decoy proteins
 cur.aff <- affinity(cur.exp, minpep = 3, avalue = "H/(H+L)",
                     use.normalized = T)
+
+## get significance for each protein and plot histogram
+
+# example without filtration
+pv <- signif_test(cur.aff, apply.chauven = F,
+                  add.plot = T, plot.sign = 1e-6)
+
+# with chauvenet filtration
+pv <- signif_test(cur.aff, apply.chauven = T, max.it = 10, ch.th = 2,
+                  add.plot = T, plot.sign = 1e-6)
+head(pv)
