4cb7d9397ca9c3eefb0e857a5fa7ab8e2c06f33a
dima <ischenko.dmitry@gmail.com>
2016-06-14 16:20:11 +0300

    main to simple

--- src/main-check.R
+++ src/main-check.R
@@ -4,16 +4,8 @@ setwd("/home/dima/pp-idirt/")
 # load libs
 source("src/R/exp-functions.R")
 
-for (e.name in c("1a", "1b", "1c", "2a", "2b", "2c")) {
-  # load exp
-  cur.exp <- idirt(file = "data/idirt/s_idirt.csv", exp.name = e.name)
-  # generate report
-  generate_report(odirt = cur.exp, wdir = sprintf("rep/%s", e.name))
-}
+# load experiment
+cur.exp <- idirt(file = "data/idirt/s_24_new.csv", "12")
 
-for (e.name in as.character(2:24)) {
-  # load exp
-  cur.exp <- idirt(file = "data/idirt/s_24_new.csv", exp.name = e.name)
-  # generate report
-  generate_report(odirt = cur.exp, wdir = sprintf("rep/%s", e.name))
-}
+# generate report
+generate_report(cur.exp, wdir = "rep/1a")
