c0f7ef9e22fcea0b94f8c138a2d15dbaefb84753
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2016-06-23 11:21:38 +0300

    affinity collection starting

---
+++ src/R/aff-collection-obj.R
@@ -0,0 +1,33 @@
+# Affinity collection object
+# It differ from simple list of affinity objects
+# because we have additional slots (e.g. affinity matrix)
+# so create separate class
+# Author: Dima Ischenko
+# Date: 23.06.2016
+
+#' Create affinity colelction from list of affinity objects
+#' 
+#' @param alist List with "affinity" objects
+#' @return "affcoll" object, collection with affinit objects
+affcollection <- function(alist) {
+  # create object
+  acoll <- list()
+  class(acoll) <- "affcoll"
+  
+  # add affinity list
+  acoll[["alist"]] <- alist
+  
+  # add matrix
+  # get all proteins
+  v.prot <- unique(unlist(lapply(alist, function(x) x$protein)))
+  # create empty matrix
+  acoll[["amtx"]] <- matrix(NA, nrow = length(v.prot),
+                                  ncol = length(alist),
+                                  dimnames = list(v.prot, names(alist)))
+  # fill with values
+  for (i in 1:length(alist)) {
+    acoll[["amtx"]][alist[[i]]$protein, names(alist)[i]] <- alist[[i]]$aff
+  }
+  
+  return(acoll)
+}
--- src/main-check.r
+++ src/main-check.r
@@ -4,20 +4,39 @@ setwd("/home/dima/pp-idirt/")
 # load libs
 source("src/R/exp-functions.r")
 source("src/R/affinity-obj.r")
+source("src/R/aff-collection-obj.R")
 
-# load experiment
-cur.exp <- mqexp(file = "data/idirt/s_24_new.csv", "14")
+# load experiments
+# getrate manually experiment names vector
+e.names <- as.character(2:24)
+# load all exp in list
+l.exp <- lapply(e.names, function(exp)
+  mqexp(file = "data/idirt/s_24_new.csv", exp))
+names(l.exp) <- e.names
+
+# get affinity object for each experiment
+#  (now only first protein for each protein group)
+#  remove contaminants and decoy proteins
+l.aff <- lapply(l.exp, function(cur.exp)
+  affinity(cur.exp, minpep = 3, avalue = "H/(H+L)", use.normalized = T))
+
+# gett affinity collection object
+cur.col <- affcollection(l.aff)
+
+# matrix with all affinity values
+head(cur.col[["amtx"]])
+
+## one experiment study
+# subset exp from list
+cur.exp <- l.exp[["2"]]
 
 # generate report
-generate_report(cur.exp, wdir = "rep/1a")
+generate_report(l.exp[[1]], wdir = "rep/1a")
 
-# get affinity object (now only first protein for each protein group)
-#  remove contaminants and decoy proteins
-cur.aff <- affinity(cur.exp, minpep = 3, avalue = "H/(H+L)",
-                    use.normalized = T)
+# get affinity object
+cur.aff <- affinity(cur.exp, minpep = 3, avalue = "H/(H+L)", use.normalized = T)
 
 ## get significance for each protein and plot histogram
-
 # example without filtration
 pv <- signif_test(cur.aff, apply.chauven = F,
                   add.plot = T, plot.sign = 1e-6)
